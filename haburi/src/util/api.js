97de547a7691734260edee7ae2e11445263ba06c
Lee SeEun <sally4405@naver.com>
2020-08-14 18:27:15 +0900

    feat: api 파라미터 수정

--- haburi/src/util/api.js
+++ haburi/src/util/api.js
@@ -1,23 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
 
-const URL = 'http://localhost:'//port번호 입력
+const URL = "http://localhost:8080"; //port번호 입력
 
 export const videoFilterAPI = async (videoNum) => {
-    try{
-        const response = await axios.post(`${URL}/api/filter`, {
-            id : videoNum
-        })
-        return response;
-    } catch(error) {
-        return {error}
-    }
-}
+  try {
+    const response = await axios.post(`${URL}/videolist`, {
+      videoUrl: videoNum,
+    });
+    return response;
+  } catch (error) {
+    return { error };
+  }
+};
 
 export const loadVideoAPI = async ({}) => {
-    try{
-        const response = await axios.get(`${URL}/api/load`);
-        return response;
-    } catch(error) {
-        return {error}
-    }
-}
+  try {
+    const response = await axios.get(`${URL}/reportvideo`);
+    console.log(response);
+    return response;
+  } catch (error) {
+    return { error };
+  }
+};
