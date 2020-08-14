8ba9ef88a49335e24d95a9fcb89caf2c33306917
Lee SeEun <sally4405@naver.com>
2020-08-14 18:28:31 +0900

    feat: 썸네일 이미지 스타일 적용

--- haburi/src/component/Tab/index.js
+++ haburi/src/component/Tab/index.js
@@ -6,6 +6,7 @@ import classnames from "classnames";
 
 import VideoThumbnail from "../VideoThumbnail";
 import { saveVideo } from "../../modules/video";
+import { videoFilterAPI } from "../../util/api";
 
 const Tab = (props) => {
   const dispatch = useDispatch();
@@ -15,33 +16,75 @@ const Tab = (props) => {
     if (activeTab !== tab) setActiveTab(tab);
   };
 
-  const list = [
-    {
-      id: 0,
-      title: "동영상0",
-      thumbnail: "https://user-images.githubusercontent.com/45891045/90220944-2b128d80-de44-11ea-9e68-bb5695d193ee.png",
-      video: "https://sightengine.com/assets/stream/examples/compilation.mp4",
-      isBlock: false,
-    },
-    {
-      id: 1,
-      title: "동영상1",
-      thumbnail: "https://user-images.githubusercontent.com/45891045/90224873-52b92400-de4b-11ea-87c6-eaefd111977b.png",
-      video: "http://video.mrporter.com/videos/productPage/173037_detail.mp4",
-      isBlock: false,
-    },
-  ];
+  // const list = [
+  //   {
+  //     id: 0,
+  //     title: "동영상0",
+  //     thumbnail: "https://user-images.githubusercontent.com/45891045/90220944-2b128d80-de44-11ea-9e68-bb5695d193ee.png",
+  //     video: "https://sightengine.com/assets/stream/examples/compilation.mp4",
+  //     isBlock: false,
+  //   },
+  //   {
+  //     id: 1,
+  //     title: "동영상1",
+  //     thumbnail: "https://user-images.githubusercontent.com/45891045/90224873-52b92400-de4b-11ea-87c6-eaefd111977b.png",
+  //     video: "http://video.mrporter.com/videos/productPage/173037_detail.mp4",
+  //     isBlock: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "블락된 동영상",
+  //     thumbnail: "https://user-images.githubusercontent.com/45891045/90224873-52b92400-de4b-11ea-87c6-eaefd111977b.png",
+  //     video: "http://video.mrporter.com/videos/productPage/173037_detail.mp4",
+  //     isBlock: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "동영상3",
+  //     thumbnail: "https://user-images.githubusercontent.com/45891045/90224873-52b92400-de4b-11ea-87c6-eaefd111977b.png",
+  //     video: "http://video.mrporter.com/videos/productPage/173037_detail.mp4",
+  //     isBlock: false,
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "동영상3",
+  //     thumbnail: "https://user-images.githubusercontent.com/45891045/90224873-52b92400-de4b-11ea-87c6-eaefd111977b.png",
+  //     video: "http://video.mrporter.com/videos/productPage/173037_detail.mp4",
+  //     isBlock: false,
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "동영상3",
+  //     thumbnail: "https://user-images.githubusercontent.com/45891045/90224873-52b92400-de4b-11ea-87c6-eaefd111977b.png",
+  //     video: "http://video.mrporter.com/videos/productPage/173037_detail.mp4",
+  //     isBlock: false,
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "동영상3",
+  //     thumbnail: "https://user-images.githubusercontent.com/45891045/90224873-52b92400-de4b-11ea-87c6-eaefd111977b.png",
+  //     video: "http://video.mrporter.com/videos/productPage/173037_detail.mp4",
+  //     isBlock: false,
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "동영상3",
+  //     thumbnail: "https://user-images.githubusercontent.com/45891045/90224873-52b92400-de4b-11ea-87c6-eaefd111977b.png",
+  //     video: "http://video.mrporter.com/videos/productPage/173037_detail.mp4",
+  //     isBlock: false,
+  //   },
+  // ];
+
+  const list = videoFilterAPI();
   dispatch(saveVideo(list));
 
-  const VideoList = list.map((video, index) => (
-    <Col sm="6">
-      <div class="card">
-        <div class="embed-responsive embed-responsive-4by3">
-          <VideoThumbnail key={index} video={video}></VideoThumbnail>
-        </div>
-      </div>
-    </Col>
-  ));
+  // const VideoList = list.map((video, index) => (
+  //   <Col sm="6" style={{ maxWidth: "30%", paddingBottom: "30px" }}>
+  //     <div class="card" style={{ height: "100%" }}>
+  //       <VideoThumbnail key={index} video={video}></VideoThumbnail>
+  //     </div>
+  //   </Col>
+  // ));
 
   return (
     <div>
@@ -94,7 +137,7 @@ const Tab = (props) => {
               <br />
               <h6>Best Video 👏</h6>
             </Col>
-            {VideoList}
+            {/* {VideoList} */}
           </Row>
         </TabPane>
         <TabPane tabId="2">
--- haburi/src/component/VideoThumbnail/index.jsx
+++ haburi/src/component/VideoThumbnail/index.jsx
@@ -1,19 +1,42 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 
 const VideoThumbnail = ({ video }) => {
   let history = useHistory();
 
   const onPassVideo = () => {
+    if (video.isBlock) {
+      alert("블락된 영상입니다. ");
+      return;
+    }
     history.push(`/streamingPage/${video.id}`);
   };
 
   return (
     <>
-      <img src={video.thumbnail} alt="" />
-      <div onClick={onPassVideo}>{video.title}</div>
+      <Wrapper onClick={onPassVideo}>
+        <ThumbnailImage src={video.thumbnail} alt="" />
+        <Title>{video.title}</Title>
+      </Wrapper>
     </>
   );
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const ThumbnailImage = styled.img`
+  width: 100%;
+  height: 80%;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  height: 20%;
+  font-size: large;
+`;
 
 export default VideoThumbnail;
