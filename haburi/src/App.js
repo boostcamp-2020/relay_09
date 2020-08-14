2644bda75315a6efb41bb7308a77fdaba6e48bdb
Lee SeEun <sally4405@naver.com>
2020-08-14 14:15:55 +0900

    chore: 구조 변경 및 불필요한 파일 삭제

--- haburi/src/App.js
+++ haburi/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import LoginPage from "./component/LoginPage";
+import Card from "./component/card";
+import MyNav from "./component/MyNav";
+import Tab from "./component/Tab";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <MyNav />
+      <LoginPage />
+      <Tab />
+      <Card />
     </div>
   );
 }
