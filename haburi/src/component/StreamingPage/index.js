933b0443b1d11a4f0d8b4f64af951ed0b9525082
Lee SeEun <sally4405@naver.com>
2020-08-14 19:53:06 +0900

    feat: 스트리밍 페이지 스타일 적용

--- haburi/src/component/StreamingPage/index.js
+++ haburi/src/component/StreamingPage/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
+
 import ReportButton from "../ReportButton";
-import "./StreamingPage.css";
+// import "./StreamingPage.css";
 import MyNav from "../MyNav";
 
 const StreamingPage = () => {
@@ -10,28 +12,47 @@ const StreamingPage = () => {
 
   const { videoNum } = useParams();
   const videoList = useSelector((state) => state.video.videoList);
+  const video = videoList && videoList.filter((v) => v.id === videoNum)[0];
 
   return (
     <>
       <MyNav />
       {videoList.length ? (
-        <div className="wrapper">
-          <video className="video" controls width="500">
-            <source src={videoList.filter((v) => v.id === videoNum)[0].video} />
+        <Wrapper>
+          <Video className="video" controls autoPlay width="500">
+            <source src={video.video} />
             Sorry, your browser doesn't support embedded videos.
-          </video>
-          <div>
-            <div className="text">{"videoList.title"}</div>
-            <div className="Reportbutton">
-              <ReportButton videoNum={videoList.filter((v) => v.id === videoNum)[0].video} className="Reportbutton" />
-            </div>
-          </div>
-        </div>
+          </Video>
+          <TitleWrapper>
+            <Text>{video.title}</Text>
+            <ReportButton videoUrl={video.video} className="Reportbutton" />
+          </TitleWrapper>
+        </Wrapper>
       ) : (
         history.push(`/components`)
       )}
     </>
   );
 };
+
+const Wrapper = styled.div`
+  width: 70%;
+  margin: auto;
+  margin-top: 5%;
+`;
+
+const Video = styled.video`
+  width: 100%;
+  height: 50%;
+`;
+
+const Text = styled.div`
+  font-size: 35px;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export default StreamingPage;
